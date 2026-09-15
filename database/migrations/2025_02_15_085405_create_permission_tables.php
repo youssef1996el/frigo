0bf8781c31a5171cb849aec220373d5ff3e18a3f
unknown <elhamrayoussef@gmail.com>
2026-09-15 22:45:42 +0100

    again

--- database/migrations/2025_02_15_085405_create_permission_tables.php
+++ database/migrations/2025_02_15_085405_create_permission_tables.php
@@ -54,7 +54,7 @@ return new class extends Migration
         Schema::create($tableNames['model_has_permissions'], static function (Blueprint $table) use ($tableNames, $columnNames, $pivotPermission, $teams) {
             $table->unsignedBigInteger($pivotPermission);
 
-            $table->string('model_type');
+            $table->string('model_type', 191);
             $table->unsignedBigInteger($columnNames['model_morph_key']);
             $table->index([$columnNames['model_morph_key'], 'model_type'], 'model_has_permissions_model_id_model_type_index');
 
@@ -77,7 +77,7 @@ return new class extends Migration
 
         Schema::create($tableNames['model_has_roles'], static function (Blueprint $table) use ($tableNames, $columnNames, $pivotRole, $teams) {
             $table->unsignedBigInteger($pivotRole);
-            $table->string('model_type');
+            $table->string('model_type', 191);
             $table->unsignedBigInteger($columnNames['model_morph_key']);
             $table->index([$columnNames['model_morph_key'], 'model_type'], 'model_has_roles_model_id_model_type_index');
         
